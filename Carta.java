ab810e307b56d2a66ea4cbb074a4d03f8a157a3b
Jhon Carrión <33500075+JhonCarrion@users.noreply.github.com>
2017-11-29 20:39:13 -0500

    Update Carta.java

--- Carta.java
+++ Carta.java
@@ -1,44 +1,44 @@
 package blackjack;
 
 /**
- * An object of type Card represents a playing card from a standard Poker deck,
- * including Jokers. The card has a palo, which can be spades, hearts, diamonds,
- * clubs, or joker. A spade, heart, diamond, or club has one of the 13 valors:
- * ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, or king. Note that "ace" is
- * considered to be the smallest valor. A joker can also have an associated
- * valor; this valor can be anything and can be used to keep track of several
- * different jokers.
+ * Un objeto de tipo Carta representa una carta de juego de un mazo de Poker estándar,
+ * incluyendo Jokers. La carta tiene un palo, que puede ser espadas, corazones, diamantes,
+ * clubes, o bromista. Una pala, corazón, diamante o club tiene uno de los 13 valors:
+ * as, 2, 3, 4, 5, 6, 7, 8, 9, 10, jota, reina o rey. Tenga en cuenta que "as" es
+ * considerado el valor más pequeño. Un comodín también puede tener un asociado
+ * valor; este valor puede ser cualquier cosa y se puede utilizar para realizar un seguimiento de varios
+ * diferentes jockers.
  */
 public class Carta {
 
-    public final static int ESPADAS = 0;   // Codes for the 4 palos, plus Joker.
+    public final static int ESPADAS = 0;   // Códigos para los 4 palos, más Joker.
     public final static int CORAZONES = 1;
     public final static int DIAMANTES = 2;
     public final static int TREBOLES = 3;
     public final static int JOKER = 4;
 
-    public final static int AS = 1;      // Codes for the non-numeric cards.
-    public final static int JOTA = 11;    //   Cards 2 through 10 have their 
-    public final static int REINA = 12;   //   numerical valors for their codes.
+    public final static int AS = 1;      // Códigos para las tarjetas no numéricas.
+    public final static int JOTA = 11;    //   Las tarjetas 2 a 10 tienen sus 
+    public final static int REINA = 12;   //  valores numéricos para sus códigos.
     public final static int REY = 13;
 
     /**
-     * This card's palo, one of the constants ESPADAS, CORAZONES, DIAMANTES,
-     * TREBOLES, or JOKER. The palo cannot be changed after the card is
-     * constructed.
+     * El palo de esta carta, una de las constantes ESPADAS, CORAZONES, DIAMANTES,
+     * TREBOLES, o JOKER. El palo no se puede cambiar después de que la tarjeta esté
+     * construido.
      */
     private final int palo;
 
     /**
-     * The card's valor. For a normal card, this is one of the valors 1 through
-     * 13, with 1 representing AS. For a JOKER, the valor can be anything. The
-     * valor cannot be changed after the card is constructed.
+     * El valor de la carta. Para una carta normal, este es uno de los valors 1 hasta
+     * 13, con 1 representando AS. Para un JOKER, el valor puede ser cualquier cosa. los
+     * el valor no se puede cambiar después de que se construye la carta.
      */
     private final int valor;
 
     /**
-     * Creates a Joker, with 1 as the associated valor. (Note that "new Card()"
-     * is equivalent to "new Card(1,Card.JOKER)".)
+     * Crea un Joker, con 1 como valor asociado. (Tenga en cuenta que "nueva tarjeta ()"
+     * es equivalente a "nueva Tarjeta (1, Tarjeta.CAJA)".)
      */
     public Carta() {
         palo = JOKER;
@@ -46,17 +46,17 @@ public class Carta {
     }
 
     /**
-     * Creates a card with a specified palo and valor.
+     * Crea una carta con palo y valor especificados.
      *
-     * @param elValor the valor of the new card. For a regular card (non-joker),
-     * the valor must be in the range 1 through 13, with 1 representing an Ace.
-     * You can use the constants Card.AS, Card.JOTA, Card.REINA, and Card.REY.
-     * For a Joker, the valor can be anything.
-     * @param elPalo the palo of the new card. This must be one of the valors
-     * Card.ESPADAS, Card.CORAZONES, Card.DIAMANTES, Card.TREBOLES, or
+     * @param elValor tel valor de la nueva carta. Para una tarjeta regular (no bromista),
+     * el valor debe estar en el rango de 1 a 13, donde 1 representa un As.
+     * Puede usar las constantes Card.AS, Card.JOTA, Card.REINA y Card.REY.
+     * Para un Joker, el valor puede ser cualquier cosa.
+     * @param elPalo el palo de la nueva carta. Este debe ser uno de los valors
+     * Card.ESPADAS, Card.CORAZONES, Card.DIAMANTES, Card.TREBOLES, o
      * Card.JOKER.
-     * @throws IllegalArgumentException if the parameter valors are not in the
-     * permissible ranges
+     * @throws IllegalArgumentException si los valors de los parámetros no están en el
+     * rangos permisibles
      */
     public Carta(int elValor, int elPalo) {
         if (elPalo != ESPADAS && elPalo != CORAZONES && elPalo != DIAMANTES
@@ -71,29 +71,29 @@ public class Carta {
     }
 
     /**
-     * Returns the palo of this card.
+     * Devuelve el palo de esta carta.
      *
-     * @returns the palo, which is one of the constants Card.ESPADAS,
-     * Card.CORAZONES, Card.DIAMANTES, Card.TREBOLES, or Card.JOKER
+     * @returns el palo, que es una de las constantes Carta.ESPADAS,
+     * Carta.CORAZONES, Carta.DIAMANTES, Carta.TREBOLES, o Carta.JOKER
      */
     public int getPalo() {
         return palo;
     }
 
     /**
-     * Returns the valor of this card.
+     * Retorna el valor de la carta.
      *
-     * @return the valor, which is one of the numbers 1 through 13, inclusive
-     * for a regular card, and which can be any valor for a Joker.
+     * @return el valor, que es uno de los números del 1 al 13, inclusive
+     * para una tarjeta regular, y que puede ser cualquier valor para un Joker.
      */
     public int getValor() {
         return valor;
     }
 
     /**
-     * Returns a String representation of the card's palo.
+     *Devuelve una representación de cadena del palo de la carta.
      *
-     * @return one of the strings "Spades", "Hearts", "Diamonds", "Clubs" or
+     * @return una de las cadenas "Picas", "Corazones", "Diamantes", "Clubes" o
      * "Joker".
      */
     public String getPaloComoString() {
@@ -112,11 +112,11 @@ public class Carta {
     }
 
     /**
-     * Returns a String representation of the card's valor.
+     * Devuelve una representación en cadena del valor de la carta.
      *
-     * @return for a regular card, one of the strings "Ace", "2", "3", ...,
-     * "10", "Jack", "Queen", or "King". For a Joker, the string is always
-     * numerical.
+     * @return para una tarjeta regular, una de las cadenas "Ace", "2", "3", ...,
+     * "10", "Jack", "Queen" o "King". Para un Joker, la cuerda es siempre
+     * numérico.
      */
     public String getValorComoString() {
         if (palo == JOKER) {
@@ -154,10 +154,10 @@ public class Carta {
     }
 
     /**
-     * Returns a string representation of this card, including both its palo and
-     * its valor (except that for a Joker with valor 1, the return valor is just
-     * "Joker"). Sample return valors are: "Queen of Hearts", "10 of Diamonds",
-     * "Ace of Spades", "Joker", "Joker #2"
+     * Devuelve una representación de cadena de esta tarjeta, que incluye tanto su palo como
+     * su valor (excepto que para un Joker con valor 1, el valor de retorno es solo
+     * "Joker"). Los valores de muestra de retorno son: "Reina de Corazones", "10 de Diamantes",
+     * "Ace of Spades", "Joker", "Joker # 2"
      */
     public String toString() {
         if (palo == JOKER) {
@@ -171,4 +171,4 @@ public class Carta {
         }
     }
 
-} // end class Card
+}
