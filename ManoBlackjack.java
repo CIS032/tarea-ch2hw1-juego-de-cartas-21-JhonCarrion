3a14d82de8e0727cd14644616ce83b408d78a9f0
Jhon Carrión <33500075+JhonCarrion@users.noreply.github.com>
2017-11-29 20:59:56 -0500

    Update ManoBlackjack.java

--- ManoBlackjack.java
+++ ManoBlackjack.java
@@ -3,44 +3,44 @@ package blackjack;
 public class ManoBlackjack extends Mano {
 
     /**
-     * Computes and returns the value of this hand in the game of Blackjack.
+     * Calcula y devuelve el valor de esta mano en el juego de Blackjack.
      */
     public int getBlackjackValor() {
 
-        int val;      // The value computed for the hand.
-        boolean as;  // This will be set to true if the
-        //   hand contains an as.
-        int cartas;    // Number of cartas in the hand.
+        int val;      // El valor calculado para la mano.
+        boolean as;  // Esto se establecerá en verdadero si la
+        // mano contiene un as.
+        int cartas;    // Número de cartas en la mano.
 
         val = 0;
         as = false;
-        cartas = getContCartas();  // (method defined in class Hand.)
+        cartas = getContCartas();  // (método definido en la clase Hand)
 
         for (int i = 0; i < cartas; i++) {
-            // Add the value of the i-th card in the hand.
-            Carta card;    // The i-th card; 
-            int cardVal;  // The blackjack value of the i-th card.
+            // Agregue el valor de la i-ésima carta en la mano.
+            Carta card;    // la i-ésima carta.
+            int cardVal;  // El valor de blackjack de la i-ésima carta.
             card = getCarta(i);
-            cardVal = card.getValor();  // The normal value, 1 to 13.
+            cardVal = card.getValor();  // El valor normal, 1 a 13.
             if (cardVal > 10) {
-                cardVal = 10;   // For a Jack, Queen, or King.
+                cardVal = 10;   // Para un JOTA, REINA o REY.
             }
             if (cardVal == 1) {
-                as = true;     // There is at least one as.
+                as = true;     // Hay al menos un as.
             }
             val = val + cardVal;
         }
 
-        // Now, val is the value of the hand, counting any as as 1.
-        // If there is an as, and if changing its value from 1 to 
-        // 11 would leave the score less than or equal to 21,
-        // then do so by adding the extra 10 points to val. 
+        // Ahora, val es el valor de la mano, contando cualquiera como 1.
+        // Si hay un as, y si cambia su valor de 1 a
+        // 11 dejaría el puntaje menor o igual a 21,
+        // entonces hazlo sumando los 10 puntos adicionales a val.
         if (as == true && val + 10 <= 21) {
             val = val + 10;
         }
 
         return val;
 
-    }  // end getBlackjackValue()
+    }
 
-} // end class BlackjackHand
+} 
